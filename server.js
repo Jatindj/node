5d0775fb0e09df0e3e6d04faa01be70cb9397705
nilesh dabhi <nileshdabhi1212@gmail.com>
2019-08-05 16:34:27 +0530

    setest

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
